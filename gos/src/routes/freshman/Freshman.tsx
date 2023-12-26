5a7c913a0d9a391c5ed45230f365ee3c00804f8d
Nikita Khvorostin <khvorostinn@gmail.com>
2023-12-26 19:11:29 +0300

    Start Freshman

--- gos/src/routes/freshman/Freshman.tsx
+++ gos/src/routes/freshman/Freshman.tsx
@@ -1,10 +1,30 @@
 import styles from "./styles.module.scss"
+import Navbar from "~/components/navbar"
+import clsx from "clsx"
 
 export default function Freshman() {
   return (
     <>
+      <Navbar />
       <main class={styles.container}>
-        <div class={styles.first}></div>
+        <div class={styles.first}>
+          <div class={styles.buttons}>
+            <div class={styles.left}>
+              <button class={styles.button}>
+                <span class={styles.text}>СВЕЧИ</span>
+              </button>
+              <button class={styles.button}>
+                <span class={styles.text}>СВЕЧИ</span>
+              </button>
+              <button class={styles.button}>
+                <span class={styles.text}>СВЕЧИ</span>
+              </button>
+            </div>
+            <button class={styles.red}>
+              <span class={styles.text}>пройти тест на профориентацию</span>
+            </button>
+          </div>
+        </div>
       </main>
     </>
   )
